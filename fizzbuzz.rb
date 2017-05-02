7fc4973f53113e5c0a68a07647b07187967cc1f0
Jan Phillip Carreon <janphillipcarreon@gmail.com>
2017-05-02 20:56:58 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(value)
+  if (value%3===0) && (value%5===0)
+  return "FizzBuzz"
+  elsif value%3===0
+    return 'Fizz'
+  elsif value%5===0
+    return 'Buzz'
+  else
+    return nil
+  end
+end
